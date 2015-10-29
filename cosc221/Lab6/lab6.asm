2705b61ae88a4446c9bc8a064e97361d414fa443
Maurice Edwards <maurice.edwards@tdameritrade.com>
2015-10-29 17:15:33 -0400

    COSC 221 - updated lab 6

--- cosc221/Lab6/lab6.asm
+++ cosc221/Lab6/lab6.asm
@@ -1,50 +1,73 @@
-.ORIG 	x3000
-
-LEA 	R0, MESG1	;Display input prompt
+; Maurice Edwards
+; COSC 221
+; Lab  6
+ 
+ 
+.ORIG   x3000
+ 
+LEA     R0, MESG1       ; Display input prompt
 PUTS
-
+ 
+; load 1st character into memory
 GETC
 OUT
-ST		R0,CHAR1
-
+ST              R0,CHAR1
+ 
+; load 2nd character into memory
 GETC
 OUT
-ST		R0,CHAR2
-
-GETC
+ST              R0,CHAR2
+ 
+; load 3rd character into memory
+GETC                            ; get character
+OUT                             ; output character
+ST              R0,CHAR3        ; save to variable
+ 
+LD              R0,CRLF         ; print new line
 OUT
-ST		R0,CHAR3
-
-LD		R0,CRLF
-OUT 
-
+ 
 LEA R0,MESG2
 PUTS
-
-LD		R0,CHAR1
-LD		R1,NEG32
-ADD		R0,R0,R1
+ 
+; print new line 
+LD              R0,CRLF
+OUT
+ 
+; display 1st character
+LD              R0,CHAR1
+LD              R1,NEG32
+ADD             R0,R0,R1
+OUT
+ 
+; print new line
+LD              R0,CRLF
+OUT
+ 
+; display 2nd character
+LD              R0,CHAR2
+LD              R1,NEG32
+ADD             R0,R0,R1
 OUT
 
-LD		R0,CHAR2
-LD		R1,NEG32
-ADD		R0,R0,R1
+; print new line
+LD              R0,CRLF
 OUT
-
-LD		R0,CHAR3
-LD		R1,NEG32
-ADD		R0,R0,R1
+ 
+; display 3rd character
+LD              R0,CHAR3
+LD              R1,NEG32
+ADD             R0,R0,R1
 OUT
-
+ 
 HALT
-
+ 
 ; Data
-NEG31 	.FILL 		#-32
-MESG1	.STRINGZ 	"Please enter your initials (First, Middle, Last)>"
-MESG2	.STRINGZ 	"Here are initials in uppercase: "
-CRLF	.FILL		#10
-CHAR1	.BLKW		1
-CHAR2	.BLKW		1
-CHAR3	.BLKW		1
-
-		.END
+NEG32   .FILL           #-32
+MESG1   .STRINGZ        "Please enter your initials (First, Middle, Last)>"
+MESG2   .STRINGZ        "Here are initials in uppercase: "
+CRLF    .FILL           #10
+CHAR1   .BLKW           1
+CHAR2   .BLKW           1
+CHAR3   .BLKW           1
+ 
+                .END
