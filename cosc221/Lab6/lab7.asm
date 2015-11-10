559d8a07d58ac0f8e746cb7b83210058757f4088
Maurice Edwards <medwar40@Maurices-MacBook-Pro.local>
2015-11-10 17:48:48 -0500

    Added comments to Lab7

--- cosc221/Lab6/lab7.asm
+++ cosc221/Lab6/lab7.asm
@@ -1,46 +1,48 @@
 ; Maurice Edwards
 ; COSC 221
+; Fall 2015
 ; Lab  7
+; this program reads in a two-digit decimal
+; number whose digits add up to less than 10
 
-.ORIG   x3000
+    .ORIG   x3000
 
-LEA     R0, MESG1       ; Display input prompt
-PUTS
+    LEA       R0, MESG2       ; Load MESG1 to R0
+    PUTS                      ; print MESG1
 
-; load 1st character into memory as a number
-GETC
-OUT
-LD R1,NEG48
-ADD             R0,R0,R1
-ST R0, NUM1
+    ; load 1st character into memory as a number
+    GETC                      ; get input character
+    OUT                       ; display entered character
+    LD        R1, NEG48       ; Load -48 into register 1
+    ADD       R1, R0, R1      ; get 'real' number and load into R1
 
+    ; get second number and add the two together
+    GETC                      ; get input character
+    OUT                       ; output input
+    ADD       R0, R0, R1      ; add both inputs
+    ST        R0, SUM         ; save to SUM variable
 
-; get second number and add the two together
-LD R1,NUM1
-GETC
-OUT
-ADD             R0,R0,R1
-ST              R0,SUM
+    ; print new line
+    LD        R0, CRLF        ; load newline character to R0
+    OUT                       ; print new line
 
-; print new line
-LD              R0,CRLF
-OUT
+    ; display display sum
+    LEA       R0, MESG2       ; Load MESG2 to R0
+    PUTS                      ; print MESG2
 
+    LD        R0, SUM         ; load SUM variable to R0
+    OUT                       ; print SUM
 
-; display display sum
-LEA R0,MESG2
-PUTS
+    ; print new line
+    LD        R0, CRLF        ; load newline character to R0
+    OUT                       ; print new line
 
-LD              R0,SUM
-OUT
+    HALT                      ; stop
 
-HALT
-
-; Data
-NEG48   .FILL           #-48
-MESG1   .STRINGZ        "Enter a two-digit decimal number>"
-MESG2   .STRINGZ        "The sum of the two numbers are: "
-CRLF    .FILL           #10
-NUM1    .BLKW           1
-SUM     .BLKW           1
-                .END
+    ; Data
+    NEG48     .FILL           #-48
+    MESG1     .STRINGZ        "Enter a two-digit decimal number>"
+    MESG2     .STRINGZ        "The sum of the two numbers are: "
+    CRLF      .FILL           #10
+    SUM       .BLKW           1
+              .END
