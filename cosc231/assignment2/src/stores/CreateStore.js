694a97feb507d6863da7ea7814248d5b1c23e48a
Maurice <medwar40@emich.edu>
2015-12-17 20:30:15 -0500

    Added createStore

--- cosc231/assignment2/src/stores/CreateStore.js
+++ cosc231/assignment2/src/stores/CreateStore.js
@@ -1,23 +1,3 @@
-var alt = require('../alt');
-
-let getAction = action => {
-    return require('../../actions/' + action);
-};
-
-export default function (name, content) {
-    let bindListeners = {};
-    content.bind.forEach(action => {
-        let a = getAction(action);
-    bindListeners.push(action.map(func => {
-            action[func] = a[func];
-}));
-
-});
-
-return alt.createStore(function () {
-    this.displayName = name;
-    this.state = content.state;
-    this.bindListeners = bindListeners;
-    this.publicMethods = content.filter(x => typeof x === 'function');
-});
-}
+/**
+ * Created by medwar40 on 12/17/15.
+ */
